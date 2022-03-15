f191b77a96ccad176fa864705b06008ea3a2311e
AR Hakim <79903756+aerhakim@users.noreply.github.com>
2022-03-15 10:18:06 +0700

    Ini commit

---
+++ app/src/main/java/id/co/pustakamulia/activity/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package id.co.pustakamulia.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.co.pustakamulia.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
